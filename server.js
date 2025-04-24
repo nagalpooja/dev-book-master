c138a0e5f1847d78847e9b3ed0790f1a3a3ac242
POOJA <pooja@Pooja-2.local>
2025-04-25 02:59:39 +0530

    Initial commit

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // SERVER
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
